b030028aa619ab715052a4ec72cf8a692f6cdaf2
Jarel Dhoman <dhomanj@gmail.com>
2018-10-20 17:22:23 -0400

    added homepage

--- favor-v7/src/app/member/dash/dash.component.ts
+++ favor-v7/src/app/member/dash/dash.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dash',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) {}
 
   ngOnInit() {
+
+  }
+
+  goHomepage() {
+    this.router.navigateByUrl('member/dash');
   }
 
 }
